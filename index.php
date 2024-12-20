123944bbf1fdd8b4916b111f2bc1ac61e0c1035f
Pavel <130083838+slotherinee@users.noreply.github.com>
2024-12-21 02:37:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include 'includes/header.php';
 ?>
 
     <main class="container mx-auto flex justify-center">
-        <h1 class="text-3xl">MAIN</h1>
+        <h1 class="text-3xl">MAIN 123</h1>
     </main>
 
 <?php include 'db/database.php' ?>
