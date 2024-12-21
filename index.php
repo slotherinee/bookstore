9e470a10e459894d52d6171e835ecc6ae8ed64cc
Pavel <p.tarasov@alef.im>
2024-12-21 22:03:00 +0300

    clean up

--- index.php
+++ index.php
@@ -6,17 +6,4 @@
     </main>
 
 <?php include "db/database.php"; ?>
-<?php $env = parse_ini_file("db/.env"); ?>
-<?php try {
-    // Hash the password securely using password_hash
-    $hashedPassword = password_hash($env["ADMIN_PASSWORD"], PASSWORD_DEFAULT); // Prepare and execute the query
-    $query = $pdo->prepare(
-        "INSERT INTO users (username, password, role) VALUES (?, ?, ?)"
-    );
-    $query->execute(["admin", $hashedPassword, "admin"]);
-    echo "Admin user created successfully.";
-} catch (PDOException $e) {
-    echo "Error creating admin user: " . $e->getMessage();
-} ?>
-
 <?php include "includes/footer.php"; ?>
