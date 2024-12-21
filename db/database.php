dc915e6ad833e60c9650c2c050cce4c1d37e7778
Pavel <p.tarasov@alef.im>
2024-12-21 21:40:34 +0300

    fix message

--- db/database.php
+++ db/database.php
@@ -8,7 +8,6 @@ try {
         $env["DB_PASS"]
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conntection to db established!";
 } catch (PDOException $e) {
     echo "Database connection failed: " . $e->getMessage();
 }
