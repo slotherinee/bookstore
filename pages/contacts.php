949f2b11be2f28ef03968fadd09c2ec811289936
Pavel <p.tarasov@alef.im>
2024-12-21 11:48:12 +0300

    Added underline to link

--- pages/contacts.php
+++ pages/contacts.php
@@ -2,7 +2,7 @@
 include "../includes/header.php"; ?>
 
     <main class="container mx-auto flex justify-center">
-        <h1 class="text-3xl">contact me at <a href="https://t.me/slotherinee">telegram</a></h1>
+        <h1 class="text-3xl">contact me at <a class="underline" href="https://t.me/slotherinee">telegram</a></h1>
     </main>
 
 <?php include "../includes/footer.php";
